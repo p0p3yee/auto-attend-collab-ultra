d949f9141cb241b1eb85ed3e0dfdad52f6881c6d
p0p3yee <owner@kagome100.com>
2020-02-10 20:38:59 +0800

    Add auto pass mic & video test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Builder, until } = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
 require("dotenv").config();
 const selector = require("./selector");
 
@@ -8,22 +9,50 @@ const netid = process.env.netid;
 const netpass = process.env.netpass;
 const courseid = "82659_1";
 
-(async function example() {
-  let driver = await new Builder().forBrowser("firefox").build();
+(async () => {
+  const driver = await new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(
+      new firefox.Options()
+        .addArguments("use-fake-device-for-media-stream")
+        .addArguments("use-fake-ui-for-media-stream")
+    )
+    .build();
+
   await driver.get(url(courseid));
+
   await driver.wait(until.elementLocated(selector.agreeTermsBtn));
   await (await driver.findElement(selector.agreeTermsBtn)).click();
+
   await driver.wait(until.elementLocated(selector.netIDLoginBtn));
   await (await driver.findElement(selector.netIDLoginBtn)).click();
+
   await doLoginBlackboard(driver);
   await doWait(3);
   await driver.switchTo().frame(0);
-  await driver.wait(until.elementLocated(selector.goInCourseRoom), 10000);
-  await (await driver.findElement(selector.goInCourseRoom)).click();
-  await driver.wait(until.elementLocated(selector.joinCourseRoom));
-  await (await driver.findElement(selector.joinCourseRoom)).click();
-  console.log("done");
+
+  await clickElementWithWait(driver, selector.goInCourseRoom);
+
+  await clickElementWithWait(driver, selector.joinCourseRoom);
+
+  await clickElementWithWait(driver, selector.coursePage.confirmVoiceBtn);
+
+  await clickElementWithWait(driver, selector.coursePage.skipVideoTest);
+
+  await clickElementWithWait(driver, selector.coursePage.skipVideoTest);
+
+  await clickElementWithWait(driver, selector.coursePage.skipVideoTest);
+
+  await clickElementWithWait(driver, selector.coursePage.tutorialLaterBtn);
+
+  await clickElementWithWait(driver, selector.coursePage.closeTutorialTipsBtn);
 })();
+
+const clickElementWithWait = async (driver, locator, timeout) => {
+  if (typeof timeout === "undefined") timeout = 10000;
+  await driver.wait(until.elementLocated(locator), timeout);
+  await (await driver.findElement(locator)).click();
+};
 
 const doWait = s => new Promise(resolve => setTimeout(resolve, 1000 * s));
 
