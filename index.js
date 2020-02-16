94a238c71b94d43f3e1fecb6b0a0e98a261efd3e
p0p3yee <owner@kagome100.com>
2020-02-16 23:55:23 +0800

    Move index to class handler

--- index.js
+++ index.js
@@ -1,59 +1,12 @@
-const { until } = require("selenium-webdriver");
 require("dotenv").config();
-const selector = require("./src/selector");
-const firefoxBuilder = require("./src/firefoxBuilder");
-const utils = require("./src/utils");
+const scheduled = require("./src/scheduled");
+const courses = require("./courses.json");
+const classHandler = require("./src/classHandler");
 
-const url = courseID =>
-  `https://learn.polyu.edu.hk/webapps/collab-ultra/tool/collabultra?course_id=_${courseID}`;
 const netid = process.env.netid;
 const netpass = process.env.netpass;
 const courseid = "82659_1";
 
 (async () => {
-  const driver = await firefoxBuilder();
-
-  await driver.get(url(courseid));
-
-  await driver.clickElementWithWait(selector.agreeTermsBtn);
-
-  await driver.clickElementWithWait(selector.netIDLoginBtn);
-
-  await doLoginBlackboard(driver);
-  await utils.doWait(3);
-  await driver.switchTo().frame(0);
-
-  await driver.clickElementWithWait(selector.goInCourseRoom);
-
-  await driver.clickElementWithWait(selector.joinCourseRoom);
-
-  await driver.clickElementWithWait(selector.coursePage.confirmVoiceBtn);
-
-  await driver.clickElementWithWait(selector.coursePage.skipVideoTest);
-
-  await driver.clickElementWithWait(selector.coursePage.skipVideoTest);
-
-  await driver.clickElementWithWait(selector.coursePage.skipVideoTest);
-
-  await driver.clickElementWithWait(selector.coursePage.tutorialLaterBtn);
-
-  await driver.clickElementWithWait(selector.coursePage.closeTutorialTipsBtn);
-
-  console.log("done");
+  console.log("Starting...");
 })();
-
-const doLoginBlackboard = async driver => {
-  try {
-    await driver.wait(until.elementLocated(selector.loginPage.netIDInput));
-    await (await driver.findElement(selector.loginPage.netIDInput)).sendKeys(
-      netid
-    );
-    await (await driver.findElement(selector.loginPage.netPassInput)).sendKeys(
-      netpass
-    );
-    await (await driver.findElement(selector.loginPage.signInBtn)).click();
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
